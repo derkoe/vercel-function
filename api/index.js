53160f650fea27263d549c1c3774077c8f468f98
Christian Köberl <derkoe@users.noreply.github.com>
2022-05-10 10:46:34 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 
 export default function handler(request, response) {
     response.status(200).json({
-        message: 'Hello, World!',
+        message: 'Hello, World from the Cloud!',
         body: request.body,
         query: request.query,
         cookies: request.cookies,
