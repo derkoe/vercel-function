b3ced8387303702eac283d53836d7051fd9870ba
Christian Köberl <christian.koeberl@gmail.com>
2025-05-19 14:18:42 +0200

    fix function return

--- api/index.js
+++ api/index.js
@@ -1,10 +1,5 @@
 // pages/api/index.js
 
 export function GET(request) {
-  return new Response({
-    message: "Hello, World for the FH Salzburg!",
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+  return  "Hello, World for the FH Salzburg!",
 }
